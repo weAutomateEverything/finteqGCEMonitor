348cbba5c32ea76c2ce4133f61a068e5b06bd609
Marc Arndt <marc@marcarndt.com>
2018-02-16 09:07:45 +0200

    adding metrix path

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ import (
 
 	"github.com/CardFrontendDevopsTeam/FinteqGCEMonitor/gceSelenium"
 	"github.com/CardFrontendDevopsTeam/FinteqGCEMonitor/gceservices"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
@@ -99,6 +100,8 @@ func main() {
 
 	mux := http.NewServeMux()
 	mux.Handle("/cutoff", cutofftimes.MakeHandler(cutoffService, httpLogger))
+	http.Handle("/", accessControl(mux))
+	http.Handle("/metrics", promhttp.Handler())
 
 	errs := make(chan error, 2)
 	go func() {
@@ -113,6 +116,20 @@ func main() {
 
 	logger.Log("terminated", <-errs)
 
+}
+
+func accessControl(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type")
+
+		if r.Method == "OPTIONS" {
+			return
+		}
+
+		h.ServeHTTP(w, r)
+	})
 }
 
 func errorEndpoint() string {
